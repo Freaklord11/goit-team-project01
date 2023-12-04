1c09bc972ba3e37d6a786fa5ff131ebefe85eb8b
Francis06131221 <francisanthonyverano@yahoo.com>
2023-12-04 20:17:27 +0800

    Update javascript of modal

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector(".order-modal-open"),
-    closeModalBtn: document.querySelector(".order-modal-close"),
-    modal: document.querySelector(".order-modal"),
+    openOrderModalBtn: document.querySelector(".order-modal-open"),
+    closeOrderModalBtn: document.querySelector(".order-modal-close"),
+    orderModal: document.querySelector(".order-modal"),
+    openSubmitModalBtn: document.querySelector(".yourordersubmit"),
+    closeSubmitModalBtn: document.querySelector(".submit-modal-close"),
+    submitModal: document.querySelector(".submit-modal"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openOrderModalBtn.addEventListener("click", toggleOrderModal);
+  refs.closeOrderModalBtn.addEventListener("click", toggleOrderModal);
+  refs.openSubmitModalBtn.addEventListener("click", submitAndToggleModals);
+  refs.closeSubmitModalBtn.addEventListener("click", toggleSubmitModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleOrderModal() {
+    refs.orderModal.classList.toggle("is-hidden");
   }
-})();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector(".submit-modal-open"),
-    closeModalBtn: document.querySelector(".submit-modal-close"),
-    modal: document.querySelector(".submit-modal"),
-  };
+  function toggleSubmitModal() {
+    refs.submitModal.classList.toggle("is-hidden");
+  }
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function submitAndToggleModals() {
+    // Here you can perform any necessary actions related to form submission
+    // For now, let's just close the submit-modal and open the order-modal
+    toggleSubmitModal();
+    toggleOrderModal();
   }
 })();
