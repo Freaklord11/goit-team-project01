8b2bc5f4f0ee8399755fb719543a22abda1f5bc4
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2023-12-11 14:53:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -74,3 +74,13 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+//change icon
+let navToggle = document.querySelector('.nav-toggle')
+let bars = document.querySelectorAll('.bar')
+
+function toggleHamburger(e) {
+  bars.forEach(bar => bar.classList.toggle('x'))
+}
+
+navToggle.addEventListener('click', toggleHamburger)
